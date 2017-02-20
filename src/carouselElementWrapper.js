788766a72918e25269f37a979be2962c5e85385d
Artem Veretennikov <Artem_Veretennikov@epam.com>
2017-02-20 12:20:11 +0400

    removed extra file

--- src/carouselElementWrapper.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const CarouselElementWrapper = ({isActive, element, maxHeight}) => {
-    return (<div>{
-        
-    }S</div>)
-}
-
-export default CarouselElementWrapper
